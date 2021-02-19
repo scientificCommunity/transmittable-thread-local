20e1344582bae49a2cd1a5419b32a48f15b9fc8c
tk <123>
2021-02-19 18:17:00 +0800

    feature: get original class

--- ttl-integrations/ttl-vertx/src/main/java/com/alibaba/ttl/agent/extension_transformlet/vertx/transformlet/NettyTtlTransformlet.java
+++ ttl-integrations/ttl-vertx/src/main/java/com/alibaba/ttl/agent/extension_transformlet/vertx/transformlet/NettyTtlTransformlet.java
@@ -37,6 +37,7 @@ public class NettyTtlTransformlet implements TtlTransformlet {
      */
     private static final String NETTY_INVOKE_CLASS_NAME = "io.netty.channel.DefaultChannelPipeline";
     private static final String NETTY_LISTENER_INVOKE_CLASS_NAME = "io.netty.channel.DefaultChannelPromise";
+    private static final String NETTY_DefaultChannelHandlerContext_UNWRAPPED_CLASS_NAME = "io.netty.channel.DefaultChannelHandlerContext";
 
     private static final String NETTY_CHANNEL_HANDLER_CLASS_NAME = "io.netty.channel.ChannelHandler";
     private static final String TTL_NETTY_CHANNEL_HANDLER_CLASS_NAME = "com.alibaba.ttl.agent.extension_transformlet.vertx.TtlNetty1Handler";
@@ -47,11 +48,14 @@ public class NettyTtlTransformlet implements TtlTransformlet {
     private static final String TTL_NETTY_HANDLER_UTIL_CLASS_NAME = "com.alibaba.ttl.agent.extension_transformlet.vertx.TtlWrapHandlerUtils";
 
     private static final Set<String> TO_BE_TRANSFORMED_CLASS_NAMES = new HashSet<>();
+    private static final Set<String> TO_BE_TRANSFORMED_UNWRAPPED_CLASS_NAMES = new HashSet<>();
     private static final Map<String, String> TO_BE_WRAPPED_CLASS_NAMES = new HashMap<>();
+
 
     static {
         TO_BE_TRANSFORMED_CLASS_NAMES.add(NETTY_INVOKE_CLASS_NAME);
         TO_BE_TRANSFORMED_CLASS_NAMES.add(NETTY_LISTENER_INVOKE_CLASS_NAME);
+        TO_BE_TRANSFORMED_UNWRAPPED_CLASS_NAMES.add(NETTY_DefaultChannelHandlerContext_UNWRAPPED_CLASS_NAME);
 
         TO_BE_WRAPPED_CLASS_NAMES.put(NETTY_CHANNEL_HANDLER_CLASS_NAME, TTL_NETTY_HANDLER_UTIL_CLASS_NAME);
         TO_BE_WRAPPED_CLASS_NAMES.put(NETTY_GENERIC_FUTURE_LISTENER_CLASS_NAME, TTL_NETTY_GENERIC_FUTURE_LISTENER_CLASS_NAME);
@@ -66,6 +70,34 @@ public class NettyTtlTransformlet implements TtlTransformlet {
             }
             classInfo.setModified();
         }
+
+        //get original class
+        if (TO_BE_TRANSFORMED_UNWRAPPED_CLASS_NAMES.contains(classInfo.getClassName())) {
+            updateConstructorForUnWrapperListener();
+        }
+    }
+
+    private void updateConstructorForUnWrapperListener() {
+        final int modifiers = method.getModifiers();
+        if (Modifier.constructorModifiers() != modifiers) {
+            return;
+        }
+
+        CtClass[] parameterTypes = method.getParameterTypes();
+        StringBuilder insertCode = new StringBuilder();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            final String paramTypeName = parameterTypes[i].getName();
+            if (TO_BE_WRAPPED_CLASS_NAMES.containsKey(paramTypeName)) {
+                String code = String.format(
+                    // decorate to TTL wrapper,
+                    // and then set AutoWrapper attachment/Tag
+                    "$%d = %s.unwrap();",
+                    i + 1, TO_BE_WRAPPED_CLASS_NAMES.get(paramTypeName));
+                logger.info("insert code before method " + signatureOfMethod(method) + " of class " + method.getDeclaringClass().getName() + ":\n" + code);
+                insertCode.append(code);
+            }
+        }
+        if (insertCode.length() > 0) method.insertBefore(insertCode.toString());
     }
 
     private void updateSetHandlerMethodsOfFutureClass_decorateToTtlWrapperAndSetAutoWrapperAttachment(CtMethod method) throws NotFoundException, CannotCompileException {
